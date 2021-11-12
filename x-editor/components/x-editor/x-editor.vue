5879e66a08f3664d959623c33bdc6ba6bf3d78b1
xue-shuai <xueshuai_12@163.com>
2021-11-12 10:18:05 +0800

    update

---
+++ x-editor/components/x-editor/x-editor.vue
@@ -0,0 +1,54 @@
+<template>
+	<view class="">
+		<view class="toolbar">
+			<text v-for="(item,index) in toolbar" :key="index" @click="setStyle(item)" style="margin-right: 6px;">{{item.text}}</text>
+		</view>
+		<editor id="xEditor" placeholder="请输入内容..."></editor>
+		<!-- 底部工具栏 -->
+		<view class="bottom">
+			<view class="list">
+				
+			</view>
+			<view class="wrap">
+				
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default{
+		data(){
+			return{
+				editor:null,
+				// type 0 普通样式   1 分割线   2 插入图片 
+				toolbar:[
+					{name:'bold',text:'粗体',type:0},
+					{name:'italic',text:'斜体',type:0},
+					{name:'underline',text:'下划线',type:0},
+					{name:'',text:'分割线',type:1},
+					{name:'',text:'插入图片',type:2},
+				]
+			}
+		},
+		mounted() {
+			uni.createSelectorQuery().select('#xEditor').context((res) => {
+				this.editor = res.context
+			}).exec()
+		},
+		methods:{
+			setStyle({name,type}){
+				if(type == 0){
+					this.editor.format(name,name)
+				}else if(type == 1){
+					this.editor.insertDivider()
+				}else if(type == 2){
+					
+				}
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ x-editor/pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="content">
+		<x-editor />
+	</view>
+</template>
+
+<script>
+	import xEditor from '@/components/x-editor/x-editor.vue';
+	export default {
+		components:{xEditor},
+		data() {
+			return {
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+</style>
